f0a76b003add124938e0daa34399bbcc6baf9eba
Raginaakhter <raginaakhter052@gmail.com>
2025-10-06 12:11:33 +0600

    add js2

---
+++ script2.js
@@ -0,0 +1,9 @@
+let num1 =12;
+
+if(num1 < 0) {
+    console.log("the number is Negative");
+} else if(num1 > 0) {
+    console.log("the number is Positive");
+} else {
+    console.log("the number is Zero");
+}
