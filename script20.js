c981a90a3286e40b4201f992ac2f58c9c9a6707f
Raginaakhter <raginaakhter052@gmail.com>
2025-10-07 17:30:55 +0600

    add script20.js

---
+++ script20.js
@@ -0,0 +1,11 @@
+let Mark = 92;
+
+if(Mark>=90){
+    console.log(" Excellent");
+}else if(Mark>=75){
+    console.log("Very Good");
+}else if(Mark>=50){
+    console.log("Average");
+}else {
+    console.log("Needs Improvement")
+};
