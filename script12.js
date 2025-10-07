86fe2a21b42145bf05e1ff29b187d9ee3cd5f383
Raginaakhter <raginaakhter052@gmail.com>
2025-10-07 16:12:39 +0600

    added script12

---
+++ script12.js
@@ -0,0 +1,14 @@
+let num1 = 105;
+let num2 = 205; 
+let optr = "*";
+
+
+if(optr ==="+"){
+    console.log(num1 + num2);
+} else if (optr === "-"){
+    console.log(num1-num2);
+} else if (optr==="*"){
+    console.log(num1*num1);
+} else if (optr==="/"){
+    console.log(num1/num1);
+}
